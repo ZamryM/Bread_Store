c0788f96c295fbd8098a7459e75065427145d105
ZamryM <133679560+ZamryM@users.noreply.github.com>
2023-06-15 11:57:56 +0530

    Add files via upload

---
+++ Model/BreadModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BreadStore.Model
+{
+    public class BreadModel
+    {
+        public string BreadName { get; set; }
+        public string ImageName { get; set; }
+        public float BreadPrice { get; set; }
+        public string BreadSize { get; set; }
+    }
+}
